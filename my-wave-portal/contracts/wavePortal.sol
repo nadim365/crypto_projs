db77508bb7939cf76b7631c2636e8dfa14d52d7b
Nadim <samnad14@gmail.com>
2022-05-01 18:39:29 -0400

    added constructor for contract WavePortal

---
+++ my-wave-portal/contracts/wavePortal.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+//SPDX = software package data exchange
+pragma solidity ^0.8.4;
+import "hardhat/console.sol";
+
+contract WavePortal{
+    constructor(){
+        console.log("This is my first contract in solidity");
+    }
+}
