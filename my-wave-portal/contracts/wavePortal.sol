19e4ffbdb419b26fe5eb15e542f64d1faf0564dc
Nadim <samnad14@gmail.com>
2022-05-09 16:14:57 -0400

    added code to store some data about who deployed the contract and to who it was deployed

--- my-wave-portal/contracts/wavePortal.sol
+++ my-wave-portal/contracts/wavePortal.sol
@@ -4,7 +4,20 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal{
+    uint256 totalWaves;
+
     constructor(){
-        console.log("This is my first contract in solidity");
+        console.log("Hello World, This is my first smart contract");
     }
+
+    function wave() public{
+        totalWaves = totalWaves + 1;
+        console.log("%s has Waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256){
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
 }
--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,17 +1,49 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners(); //grabbing wallet address of contract owner and another random address.
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); //Compile contract and generate necessary files in artifacts folder
-    const waveContract = await waveContractFactory.deploy(); // Create a local ethereum network but just for this contract.
-                                                            //after script completes, destroy the network.
-                                                           //Makes it easier to debug and test.
+
+    /*
+    Create a local ethereum network but just for this contract.
+    after script completes, destroy the network.
+    Makes it easier to debug and test.
+    Constructor runs when we deply the contract.
+    */
+    const waveContract = await waveContractFactory.deploy();
+
+
     await waveContract.deployed(); // Wait till contract is deployed, constructor runs when we deploy.
+
     console.log("Contract Deployed to: ", waveContract.address); //waveContract.address is the address of the deployed contract.
+    console.log("Contract Deployed by: ", owner.address); //returning address of the person who deploys the contract.
+
+    /*
+    when we deploy the contract, our functions become available to be called on the blockchain
+    because we used the PUBLIC keyword for the fucntions
+    */
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    /*
+    Below here is the code which simulates other people using our functions.
+    */
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
 };
 
 const runMain = async () => {
-    try{
+    try {
         await main();
         process.exit(0); // exit NOde Process without error.
-    } catch(error){
+    } catch (error) {
         console.log(error);
         process.exit(1); //exit node process while indication 'Uncaught Fatal Exception' error.
     }
