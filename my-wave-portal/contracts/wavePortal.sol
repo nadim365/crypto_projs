eaa888b5395c3d515f6f3170a29ce01e031e3a14
Nadimul Hasan <samnad14@gmail.com>
2022-06-06 16:49:19 +0000

    changed the hardhat import in wavePortal.sol to node_module/hardhat/ console.sol in settings : solidity.defaultCompiler": "localNodeModule

--- my-wave-portal/contracts/wavePortal.sol
+++ my-wave-portal/contracts/wavePortal.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: UNLICENSED
 //SPDX = software package data exchange
 pragma solidity ^0.8.4;
-import "hardhat/console.sol";
+import "../node_modules/hardhat/console.sol";
 
 contract WavePortal{
     uint256 totalWaves;
